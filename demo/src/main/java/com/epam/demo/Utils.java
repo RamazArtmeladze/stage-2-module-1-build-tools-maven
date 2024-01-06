4a3e5413c1be1751903baba71c3f80a651e3d6b6
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2024-01-06 17:55:38 +0400

    Update

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,24 @@
 package com.epam.demo;
 
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        for (String str : args) {
+            if (!isPositiveNumber(str)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPositiveNumber(String str) {
+        try {
+            double num = Double.parseDouble(str);
+            return num >= 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,16 @@
 package com.epam.utils;
 
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (str == null) return false;
+        try {
+            Float.parseFloat(str);
+        } catch (Exception e) {
+            return false;
+        }
+        return Float.parseFloat(str) > 0;
+
     }
+
 }
